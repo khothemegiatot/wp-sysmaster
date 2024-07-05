2a6149cbdff501a041a4b20d74d099eed69e6bf9
Phan Xuân Chánh <contact@phanxuanchanh.com>
2024-07-05 12:26:06 +0700

    feat: Add assets directory with CSS files, update main.php logic, and add config.php for constants
    - Added 'assets' directory containing 'css' subdirectory with CSS files
    - Modified 'main.php' to include new logic for the plugin
    - Added 'config.php' to store important constants

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+if ( !defined( 'ABSPATH' ) ) exit;
+
+define( 'ENABLE__add_quality_rating_taxonomy', false );
+
+define( 'ENABLE__add_buy_me_coffee_to_post', false );
+define( 'BUY_ME_COFFEE_ID', '');
+
--- main.php
+++ main.php
@@ -3,14 +3,18 @@
 /**
  * Plugin Name: WP Custom Codes
  * Description: A plugin containing code snippets used for system customization.
- * Version: 1.3.0
- * Author: NiranTeam
- * Author URI: https://niranteam.com/
+ * Version: 1.8.0
+ * Author: Chanh Phan Xuan
+ * Author URI: https://www.phanxuanchanh.com/
  * Network: true
- * Requires PHP: 7.4
+ * Requires at least: 5.0
+ * Tested up to: 6.5.5
+ * Requires PHP: 8.2
  */
 
 if ( !defined( 'ABSPATH' ) ) exit;
+
+require 'config.php'
 
 
 /**
@@ -21,6 +25,29 @@ function wp_custom_codes__load_textdomain() {
 }
 
 add_action( 'plugins_loaded', 'wp_custom_codes__load_textdomain' );
+
+
+/**
+ * Enqueue styles
+ */
+function wp_custom_codes__enqueue_styles() {
+    wp_enqueue_style( 'wp_custom_codes__enqueue_styles', plugin_dir_url( __FILE__ ) . 'assets/css/style.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'wp_custom_codes__enqueue_styles' );
+
+
+/**
+ * Admin enqueue styles
+ */
+function wp_custom_codes__admin_enqueue_styles() {
+    global $pagenow;
+    if ( $pagenow == 'post.php' || $pagenow == 'post-new.php' ) {
+        wp_enqueue_style( 'my-editor-style', plugin_dir_url( __FILE__ ) . 'assets/css/admin-style.css' );
+    }
+}
+
+add_action( 'admin_enqueue_scripts', 'wp_custom_codes__admin_enqueue_styles' );
 
 
 /**
@@ -87,68 +114,86 @@ add_filter( 'upgrader_package_options', 'wp_custom_codes__disable_new_plugin_and
 /**
  * Add quality rating taxonomy
  */
-function wp_custom_codes__add_quality_rating_taxonomy() {
-    $labels = array(
-        'name' => __( 'quality_rating_taxonomy__name', 'wp-custom-codes' ),
-        'singular_name' => __( 'quality_rating_taxonomy__singular_name', 'wp-custom-codes' ),
-        'search_items' => __( 'quality_rating_taxonomy__search_items', 'wp-custom-codes' ),
-        'all_items' => __( 'quality_rating_taxonomy__all_items', 'wp-custom-codes' ),
-        'parent_item' => __( 'quality_rating_taxonomy__parent_item', 'wp-custom-codes' ),
-        'parent_item_colon' => __( 'quality_rating_taxonomy__parent_item_colon', 'wp-custom-codes' ),
-        'edit_item' => __( 'quality_rating_taxonomy__edit_item', 'wp-custom-codes' ),
-        'update_item' => __( 'quality_rating_taxonomy__update_item', 'wp-custom-codes' ),
-        'add_new_item' => __( 'quality_rating_taxonomy__add_new_item', 'wp-custom-codes' ),
-        'new_item_name' => __( 'quality_rating_taxonomy__new_item_name', 'wp-custom-codes' ),
-        'menu_name' => __( 'quality_rating_taxonomy__menu_name', 'wp-custom-codes' ),
-    );
 
-    $args = array(
-        'labels' => $labels,
-        'hierarchical' => true,
-        'public' => true,
-        'publicly_queryable' => true,
-        'show_ui' => true,
-        'show_admin_column' => true,
-        'query_var' => true,
-        'show_in_rest' => true,
-        'rewrite' => array( 'slug' => 'quality-rating' ),
-    );
-
-    register_taxonomy( 'quality_rating', 'post', $args );
+if( defined( 'ENABLE__quality_rating_taxonomy' ) && ENABLE__quality_rating_taxonomy === true ) {
+    function wp_custom_codes__add_quality_rating_taxonomy() {
+        $labels = array(
+            'name' => __( 'quality_rating_taxonomy__name', 'wp-custom-codes' ),
+            'singular_name' => __( 'quality_rating_taxonomy__singular_name', 'wp-custom-codes' ),
+            'search_items' => __( 'quality_rating_taxonomy__search_items', 'wp-custom-codes' ),
+            'all_items' => __( 'quality_rating_taxonomy__all_items', 'wp-custom-codes' ),
+            'parent_item' => __( 'quality_rating_taxonomy__parent_item', 'wp-custom-codes' ),
+            'parent_item_colon' => __( 'quality_rating_taxonomy__parent_item_colon', 'wp-custom-codes' ),
+            'edit_item' => __( 'quality_rating_taxonomy__edit_item', 'wp-custom-codes' ),
+            'update_item' => __( 'quality_rating_taxonomy__update_item', 'wp-custom-codes' ),
+            'add_new_item' => __( 'quality_rating_taxonomy__add_new_item', 'wp-custom-codes' ),
+            'new_item_name' => __( 'quality_rating_taxonomy__new_item_name', 'wp-custom-codes' ),
+            'menu_name' => __( 'quality_rating_taxonomy__menu_name', 'wp-custom-codes' ),
+        );
+    
+        $args = array(
+            'labels' => $labels,
+            'hierarchical' => true,
+            'public' => true,
+            'publicly_queryable' => true,
+            'show_ui' => true,
+            'show_admin_column' => true,
+            'query_var' => true,
+            'show_in_rest' => true,
+            'rewrite' => array( 'slug' => 'quality-rating' ),
+        );
+    
+        register_taxonomy( 'quality_rating', 'post', $args );
+    }
+    
+    add_action( 'init', 'wp_custom_codes__add_quality_rating_taxonomy' );
+    
+    
+    /**
+     * Add a dropdown filter for quality rating taxonomy
+     */
+    function wp_custom_codes__quality_rating_taxonomy__dropdown_filter() {
+        global $typenow;
+    
+        if ( $typenow == 'post' ) {
+            $taxonomy = 'quality_rating';
+            $selected = isset( $_GET[$taxonomy] ) ? $_GET[$taxonomy] : '';
+    
+            $terms = get_terms( array(
+                'taxonomy' => $taxonomy,
+                'hide_empty' => true,
+            ) );
+    
+            echo '<select name="' . $taxonomy . '">';
+            echo '<option value="">' . __( 'quality_rating_taxonomy__all_items', 'wp-custom-codes' ) . '</option>';
+    
+            foreach ( $terms as $term ) {
+                $selected_attr = ( $selected == $term->slug ) ? 'selected="selected"' : '';
+                echo '<option value="' . esc_attr( $term->slug ) . '" ' . $selected_attr . '>' . esc_html( $term->name ) . '</option>';
+            }
+    
+            echo '</select>';
+        }
+    }
+    
+    function wp_custom_codes__taxonomy_filter_admin() {
+        add_action( 'restrict_manage_posts', 'wp_custom_codes__quality_rating_taxonomy__dropdown_filter' );
+    }
+    
+    add_action( 'admin_init', 'wp_custom_codes__taxonomy_filter_admin' );
 }
-
-add_action( 'init', 'wp_custom_codes__add_quality_rating_taxonomy' );
 
 
 /**
- * Add a dropdown filter for quality rating taxonomy
+ * Add buy me coffee
  */
-function wp_custom_codes__quality_rating_taxonomy__dropdown_filter() {
-    global $typenow;
 
-    if ( $typenow == 'post' ) {
-        $taxonomy = 'quality_rating';
-        $selected = isset( $_GET[$taxonomy] ) ? $_GET[$taxonomy] : '';
-
-        $terms = get_terms( array(
-            'taxonomy' => $taxonomy,
-            'hide_empty' => true,
-        ) );
-
-        echo '<select name="' . $taxonomy . '">';
-        echo '<option value="">' . __( 'quality_rating_taxonomy__all_items', 'wp-custom-codes' ) . '</option>';
-
-        foreach ( $terms as $term ) {
-            $selected_attr = ( $selected == $term->slug ) ? 'selected="selected"' : '';
-            echo '<option value="' . esc_attr( $term->slug ) . '" ' . $selected_attr . '>' . esc_html( $term->name ) . '</option>';
-        }
-
-        echo '</select>';
+if( defined( ENABLE__add_buy_me_coffee_to_post ) && ENABLE__add_buy_me_coffee_to_post === true ) {
+    function wp_custom_codes__add_buy_me_coffee_to_post($content) {
+        // Thay đổi nội dung ở đây
+        $custom_content = '<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="' . BUY_ME_COFFEE_ID . '" data-color="#FFDD00" data-emoji="" data-font="Arial" data-text="Tặng tôi một ly cà phê" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>';
+        return $content . $custom_content;
     }
+    
+    add_filter( 'the_content', 'wp_custom_codes__add_buy_me_coffee_to_post' );
 }
-
-function wp_custom_codes__taxonomy_filter_admin() {
-    add_action( 'restrict_manage_posts', 'wp_custom_codes__quality_rating_taxonomy__dropdown_filter' );
-}
-
-add_action( 'admin_init', 'wp_custom_codes__taxonomy_filter_admin' );
