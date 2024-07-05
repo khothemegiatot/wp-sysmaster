3df0676da6c6aec5dc84223152dd6a27a19eabf1
Phan Xuân Chánh <contact@phanxuanchanh.com>
2024-07-05 17:21:46 +0700

    feat: add CSS to style.css and minor changes to main.php
    Added additional CSS rules to assets/css/style.css to enhance styling. Made minor modifications in main.php, which contains the core logic of the plugin.

--- main.php
+++ main.php
@@ -189,8 +189,7 @@ if( defined( 'ENABLE__add_quality_rating_taxonomy' ) && ENABLE__add_quality_rati
  */
 
 if( defined( 'ENABLE__add_buy_me_coffee_to_post' ) && ENABLE__add_buy_me_coffee_to_post === true ) {
-    function wp_custom_codes__add_buy_me_coffee_to_post($content) {
-        // Thay đổi nội dung ở đây
+    function wp_custom_codes__add_buy_me_coffee_to_post( $content ) {
         $custom_content = '<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="' . BUY_ME_COFFEE_ID . '" data-color="#FFDD00" data-emoji="" data-font="Arial" data-text="Tặng tôi một ly cà phê" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>';
         return $content . $custom_content;
     }
